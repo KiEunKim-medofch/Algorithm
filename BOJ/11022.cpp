49c3e373e0458b7d451495a2bfde0cab0cfdf56f
KiEunKim <21900073@handong.edu>
2020-05-25 02:18:40 +0900

    20200525

---
+++ BOJ/11022.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+  int T;
+  int A, B;
+  cin >> T;
+  for(int i=0;i<T;i++)
+  {
+    cin >> A >> B;
+    cout << "Case #" << i+1 << ": " << A << " + " << B << " = " << A+B << endl;
+  }
+}
---
+++ BOJ/11718.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+  char p[100];
+  int i=0;
+  while(i!=100)
+  {
+    cin.getline(p,101);
+    cout << p << endl;
+
+  }
+  return 0;
+}
